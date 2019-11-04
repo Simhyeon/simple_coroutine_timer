d6905bc3a646f6140c5039e5ad5d3dfe926c50f1
Simhyeon <aracus755@gmail.com>
2019-11-05 01:33:00 +0900

    Added getLevelVariation method

--- app/src/main/java/com/example/coroc/CorocUtil.kt
+++ app/src/main/java/com/example/coroc/CorocUtil.kt
@@ -1,6 +1,7 @@
 package com.example.coroc
 
 import android.graphics.Point
+import android.util.Log
 import android.view.WindowManager
 import kotlinx.coroutines.delay
 
@@ -17,8 +18,10 @@ class CorocUtil {
         /*
         * 딜레이밀리세컨드 마다 어느정도의 level을 감소시켜야 하는가를 리턴함.
         * */
-        fun getLevelVariation(totalSeconds: Int, delayMilliSeconds: Int): Int {
-            return 10000 / totalSeconds / 1000 * delayMilliSeconds
+        fun getLevelVariation(totalSeconds: Int, delayMilliSeconds: Int): Float {
+            Log.d("Util", "${(10000 / totalSeconds).toFloat()} ${(10000 / totalSeconds).toFloat() / 1000}" +
+                    " ${((10000 / totalSeconds).toFloat() / 1000 * delayMilliSeconds).toInt()}")
+            return (10000 / totalSeconds).toFloat() / 1000 * delayMilliSeconds
         }
     }
 
