e71f816645f634936089747a59db80ff6fd52b45
Simhyeon <aracus755@gmail.com>
2019-11-08 22:28:28 +0900

    Added code for coroutine cancellation on exit activity.

--- app/src/main/java/com/example/coroc/WaveProgress.kt
+++ app/src/main/java/com/example/coroc/WaveProgress.kt
@@ -12,20 +12,26 @@ import kotlinx.android.synthetic.main.wave_progress.*
 
 class WaveProgress : AppCompatActivity() {
 
+    var waveTimerView: WaveTimerView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.requestWindowFeature(Window.FEATURE_NO_TITLE)
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.wave_progress)
 
-        val waveTimerView = WaveTimerView(this, waveRoot, 33, 60)
-        waveTimerView.setWaveDrawable(R.drawable.gradient_red_salvation, Color.argb(100,255,255,255), PorterDuff.Mode.SCREEN)
+        waveTimerView = WaveTimerView(this, waveRoot, 33, 120)
+        waveTimerView!!.setWaveDrawable(R.drawable.gradient_red_salvation, Color.argb(100,255,255,255), PorterDuff.Mode.SCREEN)
         //waveTimerView.setWaveDrawable(R.drawable.gradient_morpheus_den, Color.argb(0,255,255,255), PorterDuff.Mode.SRC)
-        waveTimerView.requestLayout()
+        waveTimerView!!.requestLayout()
 
-        waveTimerView.setOnClickListener {
+        waveTimerView!!.setOnClickListener {
             Toast.makeText(this, "Toggled", Toast.LENGTH_SHORT).show()
-            waveTimerView.toggleTimer()
+            waveTimerView!!.toggleTimer()
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        waveTimerView!!.endTimer()
     }
 }
