b51eba1e71d72de235f78af5a52620ccf161e472
Simhyeon <aracus755@gmail.com>
2019-11-08 22:27:57 +0900

    Changed return value to double for precision and deleted unused log

--- app/src/main/java/com/example/coroc/CorocUtil.kt
+++ app/src/main/java/com/example/coroc/CorocUtil.kt
@@ -17,11 +17,8 @@ class CorocUtil {
         /*
         * 딜레이밀리세컨드 마다 어느정도의 level을 감소시켜야 하는가를 리턴함.
         * */
-        fun getLevelVariation(totalSeconds: Int, delayMilliSeconds: Int): Float {
-            Log.d("Util", "${(10000 / totalSeconds).toFloat()} ${(10000 / totalSeconds).toFloat() / 1000}" +
-                    " ${((10000 / totalSeconds).toFloat() / 1000 * delayMilliSeconds).toInt()}")
-            return (10000 / totalSeconds).toFloat() / 1000 * delayMilliSeconds
+        fun getLevelVariation(totalSeconds: Int, delayMilliSeconds: Int): Double {
+            return (10000.0 / totalSeconds.toDouble()) / 1000.0 * delayMilliSeconds.toDouble()
         }
     }
-
 }
