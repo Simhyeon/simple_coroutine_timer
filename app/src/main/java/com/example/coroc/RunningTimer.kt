774735ea372a7d02f5cc81d00912ed9d1f071e94
Simhyeon <aracus755@gmail.com>
2019-11-16 00:31:07 +0900

    Backup 2019-11-16 Making Horse running timer view

--- app/src/main/java/com/example/coroc/RunningTimer.kt
+++ app/src/main/java/com/example/coroc/RunningTimer.kt
@@ -10,34 +10,34 @@ import kotlinx.coroutines.*
 class RunningTimer : AppCompatActivity() {
     private var isRunning = false
     lateinit var runningJob: Job
-    private val imageArray : Array<Int> = arrayOf(R.drawable.ic_running3,R.drawable.ic_running6)
+    //private val imageArray : Array<Int> = arrayOf(R.drawable.ic_running3,R.drawable.ic_running6)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.running_timer)
 
         changeImage.setOnClickListener {
-            if(isRunning){
-                runningJob.cancel()
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
-                isRunning = false
-                return@setOnClickListener
-            } else {
-                Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
-            }
-
-            isRunning = true
-            runningJob = CoroutineScope(Dispatchers.Main).launch {
-                var counter = 0
-                while (isRunning) {
-                    runningView.setImageResource(imageArray[counter])
-                    imageNumber.text = counter.toString()
-                    counter +=1
-                    if (counter > imageArray.size - 1) {
-                        counter = 0
-                    }
-                    delay(350)
-                }
-            }
+//            if(isRunning){
+//                runningJob.cancel()
+//                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+//                isRunning = false
+//                return@setOnClickListener
+//            } else {
+//                Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+//            }
+//
+//            isRunning = true
+//            runningJob = CoroutineScope(Dispatchers.Main).launch {
+//                var counter = 0
+//                while (isRunning) {
+//                    runningView.setImageResource(imageArray[counter])
+//                    imageNumber.text = counter.toString()
+//                    counter +=1
+//                    if (counter > imageArray.size - 1) {
+//                        counter = 0
+//                    }
+//                    delay(350)
+//                }
+//            }
         }
     }
 }
