e71b1341887b6a3f63da89b9d0908ee0613a9509
Simhyeon <aracus755@gmail.com>
2019-11-16 21:38:42 +0900

    Horse running graphic timer

--- app/src/main/java/com/example/coroc/CorocUtil.kt
+++ app/src/main/java/com/example/coroc/CorocUtil.kt
@@ -1,7 +1,10 @@
 package com.example.coroc
 
+import android.graphics.Color
 import android.graphics.Point
+import android.util.Log
 import android.view.WindowManager
+import kotlin.IllegalArgumentException
 
 class CorocUtil {
     companion object {
@@ -18,6 +21,46 @@ class CorocUtil {
         * */
         fun getLevelVariation(totalSeconds: Int, delayMilliSeconds: Int): Double {
             return (10000.0 / totalSeconds.toDouble()) / 1000.0 * delayMilliSeconds.toDouble()
+        }
+
+        // 시간초를 받아서 디지털 시계 시간 문자열로 변환시키는 펑션
+        fun timeToMSFormat(givenSeconds: Int) : String {
+            if (givenSeconds >= 216000) {
+                throw IllegalArgumentException("Hour string is not supported")
+            }
+
+            var minute = (givenSeconds / 60).toString()
+            var second = (givenSeconds % 60).toString()
+            var format: String
+
+            for (i in minute.length .. 1) {
+                minute = "0" + minute
+            }
+            for (i in second.length .. 1) {
+                second = "0" + second
+            }
+
+            return "$minute:$second"
+        }
+
+        fun getBlendedColor(startColor: Color, endColor: Color, ratio: Float) : Int {
+            if (ratio > 1 || ratio < 0) {
+                throw IllegalArgumentException("Ratio should be between 0 and 1")
+            }
+            var revRatio = 1 - ratio
+
+            if (revRatio == 0f) {
+                return startColor.toArgb()
+            } else if (revRatio == 1f) { // Should not be reachable
+                return endColor.toArgb()
+            }
+
+            val blendedColor = Color.valueOf(
+                (endColor.red() - startColor.red()) * revRatio + startColor.red(),
+                (endColor.green() - startColor.green()) * revRatio + startColor.green(),
+                (endColor.blue() - startColor.blue()) * revRatio + startColor.blue()
+            )
+            return blendedColor.toArgb()
         }
     }
 }
--- app/src/main/java/com/example/coroc/RunningTimer.kt
+++ app/src/main/java/com/example/coroc/RunningTimer.kt
@@ -1,18 +1,27 @@
 package com.example.coroc
 
+import android.content.res.Resources
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.running_timer.*
 import kotlinx.coroutines.*
 
 class RunningTimer : AppCompatActivity() {
+
     private var isRunning = false
     lateinit var runningJob: Job
     lateinit var timerJob: Job
+
+    var blendedColor : Int = 0
+    val givenTime: Int = 355
+    var timeLeft: Int = givenTime
+
     private val imageArray : Array<Int> = arrayOf(
         R.drawable.ic_running_horse_1,R.drawable.ic_running_horse_2,R.drawable.ic_running_horse_3,
         R.drawable.ic_running_horse_4,R.drawable.ic_running_horse_5,R.drawable.ic_running_horse_6,
@@ -25,9 +34,11 @@ class RunningTimer : AppCompatActivity() {
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.running_timer)
 
+        imageNumber.text = CorocUtil.timeToMSFormat(givenTime)
         changeImage.setOnClickListener {
             if(isRunning){
                 runningJob.cancel()
+                timerJob.cancel()
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
                 isRunning = false
                 return@setOnClickListener
@@ -38,19 +49,30 @@ class RunningTimer : AppCompatActivity() {
             isRunning = true
             runningJob = CoroutineScope(Dispatchers.Main).launch {
                 var counter = 0
-                while (isRunning) {
+                while (isRunning && timeLeft > 0) {
                     runningView.setImageResource(imageArray[counter])
-                    imageNumber.text = counter.toString() + ":00"
+                    //imageNumber.text = counter.toString() + ":00"
                     counter +=1
                     if (counter > imageArray.size - 1) {
                         counter = 0
                     }
-                    delay(100)
+                    delay(150) // 1000 / 12 하면 83.33333 이다. 1초 안에 12프레임이 모두 구현되지만 결국 오차가 생긴다.
                 }
             }
             timerJob = CoroutineScope(Dispatchers.Main).launch {
-                while(isRunning) {
+                while(isRunning && timeLeft > 0) {
+                    delay(1000) // Wait for 1 second
+                    timeLeft -=1
+                    imageNumber.text = CorocUtil.timeToMSFormat(timeLeft)
 
+                    blendedColor = CorocUtil.getBlendedColor(
+                            Color.valueOf(getColor(R.color.neonGreen)),
+                            Color.valueOf(getColor(R.color.neonRed)),
+                            timeLeft.toFloat() / givenTime
+                        )
+
+                    runningView.setColorFilter(blendedColor)
+                    imageNumber.setTextColor(blendedColor)
                 }
             }
         }
